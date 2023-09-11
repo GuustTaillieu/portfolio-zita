1d964477c98933bdd4efe0405c5105cabb1748c6
Guust Taillieu <guust@taillieusauw.net>
2023-09-11 15:37:01 +0200

    Made the basic header

---
+++ src/Components/Navigation/Navigation.tsx
@@ -0,0 +1,16 @@
+import './Navigation.scss';
+
+const Navigation = () => {
+	return (
+		<nav>
+			<a href='#'>
+				<h1>Zita Worm</h1>
+			</a>
+			<a href='#'>
+				<h2>Contact</h2>
+			</a>
+		</nav>
+	);
+};
+
+export default Navigation;
